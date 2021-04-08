ce20ed5c9d564a4517fd5e40b6be29482be2f23a
Marcin <marcingorecki1981@gmail.com>
2021-04-08 15:30:23 +0200

    music plays!!!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import './index.css'
 
 function App() {
-	const audioElement = useRef(null)
 	useEffect(() => {
-		audioElement.current.play();
+		const children = Array.from(
+			document.querySelector(`#root`).parentNode.children
+		)
+		children.find((el) => el.id === 'introMusic').play()
+		console.log(children.find((el) => el.id === 'introMusic'))
 	}, [])
 	return (
 		<div className="App">
-			<audio
-				ref={audioElement}
-				src="./assets/sounds/musicbox.mp3"
-				// src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
-				// onCanPlay={playSound}
-				// crossOrigin="anonymous"
-			></audio>
 			<div className="buttons">
-				<p id="start">
-					START
-				</p>
-				<p id="quit">QUIT</p>
-				<p id="full">FULLSCREEN</p>
+				<p id="start">HAPPY</p>
+				<p id="quit">LITTLE</p>
+				<p id="full">PEACH</p>
 			</div>
 		</div>
 	)
